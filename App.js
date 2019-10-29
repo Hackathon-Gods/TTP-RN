ed281b2cceea34d9c3dd00e3ff717f9b0c548934
synzen <michael994664@gmail.com>
2019-10-29 07:54:31 -0400

    Add styled components

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+const StyledView = styled.Text`
+  color: palevioletred;
+`
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello World!</Text>
+      <StyledView>Stylesheets are not needed with styled-components!</StyledView>
     </View>
   );
 }
